ef3ab3732d0acfdc06b56b02eed35b806b5fde85
Farhan-Nawwafal <farhan091204@gmail.com>
2026-01-28 15:24:31 +0700

    feat: login and register page

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
+use App\Http\Controllers\TestController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/register', function () {
+    return view('auth.register');
+});
+Route::get('/login', function () {
+    return view('auth.login');
 });
