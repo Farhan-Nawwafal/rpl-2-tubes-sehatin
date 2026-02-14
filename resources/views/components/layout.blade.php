288ac18daf1234a26f7408ed496d1fea9a14316e
Farhan-Nawwafal <farhan091204@gmail.com>
2026-02-14 12:52:02 +0700

    chore: temp 2 final project

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,7 +5,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Heylth App</title>
-    <script src="https://cdn.tailwindcss.com"></script>
+
+    {{-- GANTI CDN DENGAN VITE --}}
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
 <body class="bg-[#fffafa]">
